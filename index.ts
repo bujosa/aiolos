9667da1bb9af272cd411783951063c82aca3d277
bujosa <davidbujosa@gmail.com>
2023-10-25 22:13:11 -0400

    refactor: eliminate extra code and put in another file

--- index.ts
+++ index.ts
@@ -1,16 +1,15 @@
-import express, { Express, Request, Response, Application } from 'express';
+import express, { Application } from 'express';
 import dotenv from 'dotenv';
+import router from './routes';
 
-//For env File
 dotenv.config();
 
 const app: Application = express();
-const port = process.env.PORT || 8000;
+const port = process.env.PORT;
 
-app.get('/', (req: Request, res: Response) => {
-  res.send('Welcome to Express & TypeScript Server');
-});
+app.use(express.json());
+app.use(router);
 
 app.listen(port, () => {
-  console.log(`Server is Fire at http://localhost:${port}`);
+  console.log(`Server is Fire at  `);
 });
